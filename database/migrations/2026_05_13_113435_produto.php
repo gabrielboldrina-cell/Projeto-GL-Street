9a6226f33d0e4317e00bcdaf4e05b10c8f789eaa
Gabriel Boldrina <gabriel.boldrina@gmail.com>
2026-05-13 15:50:28 -0300

    Ultima atualização

--- database/migrations/2026_05_13_113435_produto.php
+++ database/migrations/2026_05_13_113435_produto.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        // 
     }
 
     /**
@@ -22,3 +22,4 @@ return new class extends Migration
         //
     }
 };
+  
